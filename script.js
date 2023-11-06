aa67e9e1d83cba9890b29386bceaa0a024512725
HamzaHersi01 <Hamza_Hersi01@hotmail.com>
2023-11-06 13:32:13 +0000

    Created errors fix em later

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let gridSize = document.getElementById("myRange").value;
+const grid = document.getElementsByClassName = "grid";
 
-for (let i = 0; i < array.length; i++) {
-    let square = document.createElement("grid-square");
-    square.appendChild(document.getElementsByClassName="grid");
+function createGrid(gridSize){
+    
+    for(let i = 0; i < gridSize; i++) {
+        const div = document.createElement("div");
+        console.log(div)
+        div.className="square";
+        grid.appendChild(document.createTextNode(div));
+    }
 }
 
+createGrid(gridSize);
