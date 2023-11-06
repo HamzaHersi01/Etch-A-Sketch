a9fbc5d03cc5ec274ca4fa1cec608aad89b6ef0e
HamzaHersi01 <Hamza_Hersi01@hotmail.com>
2023-11-06 13:11:31 +0000

    Added some js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+let gridSize = document.getElementById("myRange").value;
+
+for (let i = 0; i < array.length; i++) {
+    let square = document.createElement("grid-square");
+    square.appendChild(document.getElementsByClassName="grid");
+}
 
